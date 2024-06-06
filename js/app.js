60229cc276bbcb225fbf25108d245c7804090376
Násser Yousef Ali <nasser.yousef.unb@gmail.com>
2024-06-06 00:41:46 -0300

    First commit, update content and project.

---
+++ js/app.js
@@ -0,0 +1,128 @@
+/*==================== SHOW MENU ====================*/
+/*const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    // Validate that variables exist
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('active-menu-mobile');
+            toggle.classList.toggle('active-bx');
+        })
+    }
+}*/
+
+
+//showMenu('bx','menu-mobile');
+
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+
+/*==================== ANIMATION TEXT HERO ====================*/ 
+
+// Função genérica
+function minhaFuncao() {
+    var textWrapper = document.querySelector('.ml2');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({loop: true})
+    .add({
+        targets: '.ml2 .letter',
+        scale: [4,1],
+        opacity: [0,1],
+        translateZ: 0,
+        easing: "easeOutExpo",
+        duration: 950,
+        delay: (el, i) => 70*i
+    }).add({
+        targets: '.ml2',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
+}
+
+/*==================== GSAP ====================*/  
+
+/*--=========== GSAP SCROLLTRIGGER ============- */
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    /*--=========== TIMELINE LOADING E HERO ============- */
+    var tl1 = gsap.timeline();
+
+
+    tl1
+    .to('.screen-loading', {
+        duration: 2,
+        opacity: 0, 
+        delay: 4,
+        ease: "power4.out",
+    })
+    .from("header div", {
+        duration: 2, 
+        opacity: 0, 
+        stagger:{each: 0.2},
+        ease: "power4.out",
+    }, '-=1.5')
+    .from(".spline", {
+        duration: 1, 
+        opacity: 0,
+        y: 200,
+        ease: "power4.out",
+    }, '-=0.2')
+    .from(".hero div", {
+        duration: 2,  
+        opacity: 0, 
+        y: 200,
+        stagger:{each: 0.1},
+        ease: "power4.out",
+    }, '-=1.9')
+    .from("#text-blocks span", {
+        duration: 2,  
+        opacity: 0, 
+        x: -200,
+        stagger:{each: 0.2},
+        ease: "power4.out",
+    }, '-=1.9')
+    .from("#text-unleash span", {
+        duration: 1, 
+        opacity: 0, 
+        x: -50,
+        stagger:{each: 0.1},
+        ease: "power4.out",
+    }, '-=1.9')
+
+
+
+    /*--=========== ANIMATION TEXTS PRESENTATION ============- */
+    gsap.from(".text-presentation-design",{
+        x: 110,
+        duration: 1.5,
+        ease: "power4.out",
+        scrollTrigger:{
+            trigger: '.text-presentation-design',
+            start: 'top 70%',
+            end: 'bottom 30%',
+            scrub: 3,
+        }
+    });
+
+    
+
+});
+
+//ScrollTrigger.addEventListener("scrollStart", () => ScrollTrigger.refresh() );
+
+
+
+    
+
+
---
+++ readme.md
@@ -0,0 +1,49 @@
+# Portfólio de Násser Yousef
+
+📜 Este é o portfólio de Násser Yousef Ali, é o meu portfólio (https://nyousefali.com.br/). É por aqui que divulgo um resumo sobre mim e o link
+dos meus projetos e trabalhos. 
+
+<p align="center">
+<img src="svg/metatag-ny.webp" alt="Metatag Nasser Yousef, está imagem é a logo do Násser Yousef" border="0">
+</p>
+
+
+💡 Basicamente, trata-se de um website que visa aprimorar o entendimento de 
+realizar layouts mais entusiastas, alta fidelidade e variedade de animações.
+É o projeto mais completo, utiliza de todos os recursos de layout como position,
+flexbox, grid system com SASS, GSAP para animações etc.
+
+* HTML5
+* JSES6+
+* SCSS (https://sass-lang.com/)
+* GSAP (https://greensock.com/)
+* Figma (UI Design) (https://figma.com/)
+
+## 🛠 Instalação
+
+Windows, MacOs ou Linux: É um projeto HTML, CSS e JS puros, basta abrir 
+o projeto com algum editor de código e abrir por meio de um servidor simples 
+como o LiveServer do VsCode.
+
+
+## 📈 Implementação do projeto
+
+A implementação do projeto voce pode visualizar clicando aqui >> https://nyousefali.com.br/
+
+## 💻 Configuração para Desenvolvimento
+
+Por ser um website feito com as tecnologias nativas suportadas por browsers, 
+não é necessário nenhuma configuração para rodar o projeto. Basta que seja aberto 
+em um editor de código e um servidor simples. Indico utilizar o VSCode e 
+instalar a extensão "Live Server" (https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer) para ser criado um servidor local e ter a funcionalidade do reloading
+ao salvar o projeto.
+
+## 🚀 Contribuição
+
+1. Conheça os outros projetos do curso em FrontPUSH 2.0 (<https://frontpush.com.br>)
+2. Veja mais projetos gratuitos no GitHub (<https://github.com/nyousefali>)
+3. Veja alguns outros projetos no meu portfólio (<https://nyousefali.com.br>)
+4. Tem alguma dúvida? Me mande uma DM no Instagram (<https://www.instagram.com/nasseryousef__/>)
+5. Mais projetos de Design, veja o Dribbble (<https://dribbble.com/nyousefali>)
+6. Mais códigos de animações gratuitos? Veja no Codepen (<https://codepen.io/nyousefali>)
+7. Muito obrigado por estar aqui! \o/
